4bc9d27973de4e5da0867007890bbbc9396235fe
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-06-25 10:56:20 +0530

    new file added

--- nested.py
+++ nested.py
@@ -3,39 +3,81 @@
         print(j,end="\t")
     print()"""
 
-a=1
+"""a=1
 for i in range(3):
     for j in range(3):
         print(a,end="\t")
         a+=1
+    print()"""
+
+
+"""for i in range(1,4):
+    for j in range(i):
+        print(i-1,end="\t")
+    print()"""
+
+"""a=0
+for i in range(1,4):
+    for j in range(i):
+        print(a,end="\t")
+        a+=1
+    print()"""
+
+"""for i in range(1,4):
+    a=i-1
+    for j in range(i):
+        print(a,end="\t")
+        a-=1
+    print()"""
+
+"""a=65
+for i in range(3):
+    for j in range(4):
+        print(chr(a),end="\t")
     print()
+    a+=1"""
+
+
+"""a=65
+for i in range(3):
+    for j in range(3):
+        print(chr(a),end="\t")
+        a+=1
+    print()"""
 
 
 
+"""for i in range(3):
+    a=65
+    for j in range(3):
+        print(chr(a),end="\t")
+        a+=1
+    print()"""
+
+
+"""a=65
+for i in range(1,4):
+    for j in range(i):
+        print(chr(a),end="\t")
+        a+=1
+    print()"""
 
 
 
+"""for i in range(1,4):
+    a=65
+    for j in range(i):
+        print(chr(a),end="\t")
+        a+=1
+    print()"""
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+a=65
+for i in range(1,4):
+    for j in range(i):
+        print(chr(a),end="\t")
+    print()
+    a+=1
 
 
 

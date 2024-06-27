757cd1e76ae5d7cb1dd753d3195cb9790c7660af
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-06-27 11:08:48 +0530

    new file added

--- List/stud.py
+++ List/stud.py
@@ -1,2 +1,20 @@
-names=[]
-a=input("enter how many students")
+# names=[]
+# a=int(input("enter how many students"))
+# for i in range(a):
+#     name=input("enter the name")
+#     names.append(name)
+# print(names)
+
+
+# l=[1,2,3,1,2,3]
+# a=[]
+# for i in l:
+#     if i not in a:
+#         a.append(i)
+# print(a)
+
+l=["manu","akhil","sanu"]
+for i in l:
+    print(i)
+
+

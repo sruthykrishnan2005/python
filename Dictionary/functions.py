852bd53c2c376969951a0e4526a8556ba29e5d13
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-07-02 11:26:23 +0530

    new file added

---
+++ Dictionary/functions.py
@@ -0,0 +1,12 @@
+# print('start')
+# def fun1():
+#     print('welcome')
+# print('after')
+# fun1()
+
+
+print('start')
+def fun1():
+   print('start')
+def fun1():
+    print('welcome')
--- Dictionary/numbers.py
+++ Dictionary/numbers.py
@@ -1,4 +1,13 @@
 num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
 a=int(input("enter the number"))
-for i in range():
+b=str(a)
+result=''
+while a>0:
+    d=a%10
+    word=num[d]
+    result=word+''+result
+    a=a//10
+    print(result)
+
+
     

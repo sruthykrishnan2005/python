6023554d41770ad9c81ce183748fe5de84c01a28
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-07-05 10:12:52 +0530

    new file added

---
+++ List/tuple.py
@@ -0,0 +1,20 @@
+# t=(10,2,5,6)
+# print(t.index(5))
+# print(t.count(10))
+
+# a=10,11,12
+# print(a)
+
+# a=(10)
+# print(a)
+
+# a=(10,)
+# print(a)
+
+
+# t=(1,2,3,4)
+# l=list(t)
+# print(l)
+# l.append(5)
+# t=tuple(l)
+# print(t)

c0c18c083486f106b99540dc37cea69743e207c6
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-07-06 10:16:22 +0530

    new file added

---
+++ module/main.py
@@ -0,0 +1,6 @@
+from reg import *
+data=[]
+while True:
+    print("1.add \n 2.display \n 3.update \n 4.delete \n 5.exit \n") 
+    ch=int(input("enter your choice"))
+    
---
+++ module/reg.py
@@ -0,0 +1,7 @@
+def register(data):
+    name=input("enter the name")
+    age=int(input("enter age"))
+    id=int("enter id")
+    place=input("enter place")
+    data={name,age,id,place}
+    

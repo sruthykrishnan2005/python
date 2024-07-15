ca99c6d4c7c177cbe678bee6168135042b796cca
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-07-15 11:59:58 +0530

    new file added

--- oops/inheritance.py
+++ oops/inheritance.py
@@ -103,34 +103,35 @@
 
 
 
-class A:
-    def a1(self):
-        print('a1')
-class B:
-    def b1(self):
-        print('b1')
-class F:
-    def f1(self):
-        print('f1')
-class C(A,B):
-    def c1(self):
-        print('c1')
-class D(B,F):
-    def d1(self):
-        print('d1')
-class E(C,D):
-    def e1(self):
-        print('e1')
-sanu=A()
-sanu.a1()
-abc=B()
-abc.b1()
-anu=C()
-anu.c1()
-manu=D()
-manu.d1()
-appu=E()
-appu.e1()
-ammu=F()
-ammu.f1()
+# class A:
+#     def a1(self):
+#         print('a1')
+# class B:
+#     def b1(self):
+#         print('b1')
+# class F:
+#     def f1(self):
+#         print('f1')
+# class C(A,B):
+#     def c1(self):
+#         print('c1')
+# class D(B,F):
+#     def d1(self):
+#         print('d1')
+# class E(C,D):
+#     def e1(self):
+#         print('e1')
+# sanu=A()
+# sanu.a1()
+# abc=B()
+# abc.b1()
+# anu=C()
+# anu.c1()
+# manu=D()
+# manu.d1()
+# appu=E()
+# appu.e1()
+# ammu=F()
+# ammu.f1()
+
 

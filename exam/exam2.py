786bea63020d31e59b11e4a0d1e20de7e199470a
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-10-19 11:27:53 +0530

    examadded

---
+++ exam/exam2.py
@@ -0,0 +1,44 @@
+# //1 2 3
+# 4 5
+# 6//
+
+# a=1
+# for i in range(3):
+#     for j in range(3-i):
+#         print(a,end='\t')
+#         a+=1
+#     print()
+
+
+
+
+# //A B C
+# A B 
+# A//
+
+# for i in range(3):
+#     a=65
+#     for j in range(3-i):
+#         print(chr(a),end='\t')
+#         a+=1
+#     print()
+
+
+
+# //1 2 1
+# 1 2 1
+# 1 2 1//
+
+
+
+# for i in range(3):
+#     for j in range(3):
+#         if j==1:
+#             print(2,end='\t')
+#         else:
+#             print(1,end='\t')
+#     print()
+
+
+
+

2b9780806ca7acbeca7a2db67f0190c7285d5b03
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-11-02 11:23:19 +0530

    examadded

---
+++ exam/exam3.py
@@ -0,0 +1,49 @@
+#//LARGEST STRING// 
+
+# l=['hello','welcome']
+
+# largest_word=''
+# max_length=0
+
+# for i in l:
+#     if len(i)>max_length:
+#         max_length=len(i)
+#         largest_word=i
+# print("largest_word:",largest_word)
+
+
+#//SMALLEST STRING//
+
+# Data = ['helo', 'welcome', 'hai']
+
+# smallest_word = Data[0]
+# min_length = len(smallest_word)
+
+# for word in Data:
+#     if len(word) < min_length:
+#         min_length = len(word)
+#         smallest_word = word
+
+# print("smallest word:",smallest_word)
+
+
+#//LARGEST NO//
+
+# l=[1,2,3,4,10,5]
+# n=l[0]
+# for i in l:
+#     if i>n:
+#         n=i
+# print("largest no:",n)
+
+
+#//STRING REVERSE//
+
+# a='hallo'
+# b=''
+# i=len(a)-1
+
+# while i>=0:
+#     b +=a[i]
+#     i-=1
+# print(b)
